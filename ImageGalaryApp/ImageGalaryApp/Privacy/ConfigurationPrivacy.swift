af7aed95683b2031a0bca23caa461c76262b7887
Nikita Moskalenko <86294757+NikitaMoskalenko@users.noreply.github.com>
2024-01-03 19:22:43 +0300

    Initial commit

---
+++ ImageGalaryApp/ImageGalaryApp/Privacy/ConfigurationPrivacy.swift
@@ -0,0 +1,31 @@
+//
+//  ConfigurationPrivacy.swift
+//  ImageGalaryApp
+//
+//  Created by Nikita Moskalenko on 3.01.24.
+//
+
+import Foundation
+public enum ConfigurationPrivacy {
+
+    enum Keys {
+        static let baseUrl = "BASE_URL"
+        static let accessKey = "ACCESS_KEY"
+    }
+
+    private static let infoPlist: [String : Any] = {
+        guard
+            let dict = Bundle.main.infoDictionary
+        else { fatalError() }
+
+        return dict
+    }()
+
+    static let base_url: String = {
+        guard
+            let baseUrl = ConfigurationPrivacy.infoPlist[Keys.baseUrl] as? String
+        else { fatalError() }
+
+        return baseUrl
+    }()
+}
---
+++ ImageGalaryApp/ImageGalaryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageGalaryApp
+//
+//  Created by Nikita Moskalenko on 3.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
